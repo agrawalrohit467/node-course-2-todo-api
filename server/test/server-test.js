0a8ad438ce9c2551b340aed8759beac8c0fcb711
Rohit Agrawal <agrawalrohit467@gmail.com>
2017-09-22 10:17:07 +0530

    Added GET /todos route

---
+++ server/test/server-test.js
@@ -0,0 +1,10 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../modules/todo');
+
+// describe('POST /todos', () => {
+
+// })
+//incomplete
